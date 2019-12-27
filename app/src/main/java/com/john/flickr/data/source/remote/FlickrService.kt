89d5c64ab5cff57661c302225b12313e0bfc3e28
johnsung419 <hsung@geico.com>
2019-12-27 15:40:09 -0500

    adding a calling url

--- app/src/main/java/com/john/flickr/data/source/remote/FlickrService.kt
+++ app/src/main/java/com/john/flickr/data/source/remote/FlickrService.kt
@@ -4,6 +4,10 @@ import com.john.flickr.data.FlickrResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
+/**
+ *
+ * https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=300&format=json&api_key=f0e6fbb5fdf1f3842294a1d21f84e8a6&text=kitten
+ */
 
 interface FlickrService {
     @GET("/services/rest/")
